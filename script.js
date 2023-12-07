b8e64361e42f313c254ac778213366619f7091ad
Uwimbabazi <uwimbabazidorcas65@gmail.com>
2023-12-07 17:44:05 +0200

    Add description

--- script.js
+++ script.js
@@ -19,10 +19,10 @@ const works = [
     title: 'To do List',
     image: 'img/todo.png',
     imgeAlt: 'To do list image',
-    canopy: ['CANOPY', 'Front-End Dev', '2022'],
+    canopy: ['CANOPY', 'Front-End Dev', '2023'],
     descript:
-    'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    languages: ['html', 'css', 'javascript'],
+    'In this project, I added some functionality to your application to make it interactive. The user will also be able to mark task completion by selecting the corresponding checkbox (or undo it by unchecking the checkbox). The updated tasks list will be stored in local storage.',
+    languages: ['html', 'css', 'javascript', 'webpack'],
     sourcebtn: '<a href="https://github.com/Dorcas126/To-Do-List">See source</a>',
     liveBtn: '<a href="https://dorcas126.github.io/To-Do-List/dist/">See live</a>',
   },
@@ -31,9 +31,9 @@ const works = [
     title: 'Awesome Books',
     image: 'img/awesome.png',
     imageAlt: 'snapshoot Portfolio picture',
-    canopy: ['FACEBOOK', 'Back End Dev', '2015'],
+    canopy: ['FACEBOOK', 'Back End Dev', '2023'],
     descript:
-      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      'In this Awesome books Es6 Create a new directory and initialize it with git. Use npm init -y command to create package.json file. Create the entry point for your JavaScript code called index.js Create index.css file for your styles. Create the main index.html file. Link your index.js and index.css files there. Create a directory called modules.',
     languages: ['html', 'css', 'Javascript'],
     sourcebtn: '<a href="https://github.com/Dorcas126/Awesome-book-project">See source</a>',
     liveBtn: '<a href="https://dorcas126.github.io/Awesome-book-project/">See live</a>',
@@ -43,9 +43,9 @@ const works = [
     title: 'Space travelers',
     image: 'img/traveller.png',
     imageAlt: 'snapshoot Portfolio picture',
-    canopy: ['FACEBOOK', 'Full Stack Dev', '2015'],
+    canopy: ['FACEBOOK', 'Full Stack Dev', '2023'],
     descript:
-    'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+    'In this project, we will be working with real live data from the SpaceX API to build a web application for a company that provides commercial and scientific space travel services. The application will allow users to book rockets and join selected space missions.',
     languages: ['html', 'css', 'Javascript'],
     sourcebtn: '<a href="https://github.com/Dorcas126/space-travelers">See source</a>',
     liveBtn: '<a href="https://dorcaskikospacetravelers.netlify.app/">See live</a>',
@@ -55,9 +55,9 @@ const works = [
     title: 'Movie App',
     image: 'img/movie.png',
     imageAlt: 'snapshoot Portfolio picture',
-    canopy: ['Uber', 'Lead Developer', '2018'],
+    canopy: ['Uber', 'Lead Developer', '2023'],
     descript:
-    'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    'Built using JavaScript, HTML, and CSS, with webpack as the build tool. It makes use of the TvmazeAPI to retrieve information about each movies, and also utilizes a custom API to store and retrieve comments.',
     languages: ['html', 'css', 'Javascript'],
     sourcebtn: '<a href="https://github.com/Dorcas126/JavaScript-capstone-project">See source</a>',
     liveBtn: '<a href="https://dorcas126.github.io/JavaScript-capstone-project/dist/">See live</a>',
@@ -81,7 +81,7 @@ for (let i = 0; i < works.length; i += 1) {
           <p> ${work.descript}</p>
           <ul>
               <li class="html-tag">${work.languages[0]}</li>
-              <li class="css-tag">${work.languages[1]}css</li>
+              <li class="css-tag">${work.languages[1]}</li>
               <li class="js-tag">${work.languages[2]}</li>
           </ul>
           <button type="button" id="see_project">${work.liveBtn}</button>
